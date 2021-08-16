06d50ab3c66d7c3cb1b7ac723a4d1fe0e8616280
Puruhithika Kunde <puruhithika.kunde@tokusystems.com>
2021-08-15 19:39:30 -0600

    Results tabulated and Date and last readings formatted

--- latest_readings.py
+++ latest_readings.py
@@ -1,18 +1,41 @@
 import psycopg2
-conn = psycopg2.connect(host="apidemo.tokusystems.com",port="5432",dbname="tsdb",user="data_viewer",password="tokuapidemosystems")
-cur = conn.cursor()
-cur.execute('''SELECT a.name, h.name, s.name, sd.t, sd.y
-FROM assets a
-JOIN hardpoints h ON a.id = h.asset_id
-JOIN signals s ON h.id = s.hardpoint_id
-JOIN LATERAL (
-    SELECT x.t, x.y
-    FROM signal_data x
-    WHERE x.signal_id = s.id
-    ORDER BY x.t DESC
-    LIMIT 1
-) sd ON true''')
-query_results = cur.fetchall()
-print(query_results)
-cur.close()
-conn.close()
+from math import log10, floor
+from tabulate import tabulate
+import datetime
+
+
+def round_sig(x, sig=5):
+    return round(x, sig-int(floor(log10(abs(x))))-1)
+
+try:
+    conn = psycopg2.connect(host="apidemo.tokusystems.com",port="5432",dbname="tsdb",user="data_viewer",password="tokuapidemosystems")
+    cur = conn.cursor()
+    cur.execute('''SELECT a.name, h.name, s.name, sd.t, sd.y
+    FROM assets a
+    JOIN hardpoints h ON a.id = h.asset_id
+    JOIN signals s ON h.id = s.hardpoint_id
+    JOIN LATERAL (
+        SELECT x.t, x.y
+        FROM signal_data x
+        WHERE x.signal_id = s.id
+        ORDER BY x.t DESC
+        LIMIT 1
+    ) sd ON true''')
+    query_results = cur.fetchall()
+    newArray = []
+    for row in query_results:
+        newRow = []
+        for value in row:
+            if isinstance(value,float)and value!= 0.0 :
+                newRow.append(round_sig(value,5))  
+            elif isinstance(value,datetime.datetime):
+                newRow.append( value.strftime('%c'))
+
+            else:
+                newRow.append(value)
+            newArray.append(newRow)
+
+    print(tabulate(newArray,headers=["Hard point","Asset name", "Signal name","Last Time","Last reading"]))
+finally:    
+    cur.close()
+    conn.close()
