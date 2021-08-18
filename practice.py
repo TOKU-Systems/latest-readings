297a2c93bfc88a703ddf5a1deac535333d36408c
Puruhithika Kunde <puruhithika.kunde@tokusystems.com>
2021-08-18 10:43:25 -0600

    deelted my practice file

--- practice.py
+++
@@ -1,7 +0,0 @@
-def height_calculation(pres,acceleration_due_to_gravity,specific_gravity):
-    height = pres/(acceleration_due_to_gravity * specific_gravity)
-    return height
-
-
-h=height_calculation(100,9.8,9)
-print(h)
